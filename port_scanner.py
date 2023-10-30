8ba5caf97034d77a5757886daf80c65040dbeeff
Osito826 <rssmky1@yahoo.com>
2023-10-30 22:21:14 +0000

    completed project

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,55 @@
 import socket
+import re
+import common_ports
 
-def get_open_ports(target, port_range):
+
+def get_open_ports(target, port_range, verbose_mode=False):
+    ip = ""
     open_ports = []
+    hostnamenotfound = False
+    try:
+      ip = socket.gethostbyname(target)
+      for port in range(port_range[0], port_range[1]+1):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        
+        result = s.connect_ex((ip, port))
+        if result == 0:
+          open_ports.append(port)
+        s.close()
+    
+    except KeyboardInterrupt:
+      return "Exiting program"
+    except socket.gaierror:
+      if(re.search("[a-zA-Z]", target)):
+        return "Error: Invalid hostname"
+      return "Error: Invalid IP address"
+    except socket.error:
+      return "Error: Invalid IP address"
 
-
-
-    return(open_ports)
+    host = None
+    try:
+      host = socket.gethostbyaddr(ip)[0]
+    except socket.herror:
+      host = None
+      hostnamenotfound = True
+  
+    #returned_string = "Open port for"
+    if not verbose_mode:
+      print(open_ports)
+      return (open_ports)
+    else:
+      returned_string = ""
+      returned_string += f"Open ports for {host} ({ip})\n"
+      if hostnamenotfound : 
+        returned_string = f"Open ports for {ip}\n"
+      returned_string += "PORT     SERVICE\n"
+    
+      for port in open_ports:
+        spacing = "".center(9-len(str(port)))
+        returned_string += f'{port}{spacing}{common_ports.ports_and_services[port]}\n'
+  
+      return returned_string.rstrip("\n")
+  
+    return (open_ports)
+  
